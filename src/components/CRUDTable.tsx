8e0d46b447f6d3c237f16168b38e4250e1380e84
Lorenzo Banks <mainedoceducation@gmail.com>
2023-07-29 23:14:15 -0400

    feat: Added CRUDTable Props For Adding Extra Actions As MenuItems as Well As The Code To Support That Menu.

--- src/components/CRUDTable.tsx
+++ src/components/CRUDTable.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
 TableContainer,
 Table,
@@ -9,16 +10,24 @@ Paper,
 Typography,
 IconButton,
 Button,
+Menu,
+MenuProps,
 Tooltip,
 } from '@mui/material';
+
+import {
+	styled,
+	alpha
+} from '@mui/material/styles';
+
 import { Stack } from '@mui/system';
 
 import {
-	ArrowDropDown,
-    Delete,
-	More,
-	MoreHoriz,
-Update, ViewList 
+ArrowDropDown,
+Delete,
+MoreHoriz,
+Update, 
+ViewList 
 } from '@mui/icons-material';
 
 // Import Styles ----------------------------
@@ -32,27 +41,86 @@ interface DataItemType {
 interface CRUDTableProps<T extends DataItemType> {
 	data:T[];
 	index:keyof T & string;
+	additionalActions?:React.ReactNode;
 }
 
-function CRUDTable<T extends DataItemType>({data, index}:CRUDTableProps<T>)	{
+
+const MoreActionsMenu = styled((props: MenuProps) => (
+	<Menu
+		elevation={0}
+		anchorOrigin={{
+			vertical:'bottom',
+			horizontal:'right',
+		}}
+		transformOrigin={{
+			vertical:'top',
+			horizontal:'right',
+		}}
+		{...props}
+	/>
+))(({ theme }) => ({
+	'& .MuiPaper-root': {
+		borderRadius: 6,
+		marginTop:theme.spacing(1),
+		minWidth:180,
+		color:
+			theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+		boxShadow:
+			'rgb(255,255,255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+		'& .MuiMenu-list':{
+			padding: '4px 0',
+		},
+		'& .MuiMenuItem-root': {
+			'& .MuiSvgIcon-root':{
+				fontSize:18,
+				color: theme.palette.text.secondary,
+				marginRight: theme.spacing(1.5),
+			},
+			'&:active':{
+				backgroundColor: alpha(
+					theme.palette.primary.main,
+					theme.palette.action.selectedOpacity,
+				),
+			},
+		},
+	}
+}));
+
+function CRUDTable<T extends DataItemType>({data, index, additionalActions}:CRUDTableProps<T>)	{
+
+	const [moreActionsMenuAnchor, setMoreActionsMenuAnchor] = React.useState<null | HTMLElement>(null) 
+	const moreActionsMenuOpen = Boolean(moreActionsMenuAnchor);
+	const handleMoreActionsMenuClick = (event:React.MouseEvent<HTMLElement>) => {
+		setMoreActionsMenuAnchor(event.currentTarget);
+	};
+	const handleCloseMoreActionsMenu = () => {
+		setMoreActionsMenuAnchor(null);
+	};
+
 
 	return (
 		<TableContainer component={Paper}>
 			<Table>
 				<TableHead>
-					<TableRow>
+					<TableRow className='crudtbl-header-row'>
+						{/* Figure Out The Header Names, Based On The DataType Keys -- */}
 						{data[0] && Object.keys(data[0]).map((header) => header !== index && (
-							<TableCell key={header}><Typography variant="overline" fontWeight={700}>{header.toUpperCase()}</Typography></TableCell>
+							<TableCell key={header} className='crudtbl-header-cell'>
+								<Typography variant="overline" fontWeight={700}>
+									{header.toUpperCase()}
+								 </Typography>
+							</TableCell>
 						))}
-						<TableCell align='right'  colSpan={2}><Typography variant='overline' fontWeight={700}>ACTIONS</Typography></TableCell>
+						<TableCell align='right' className='crudtbl-header-actions-cell'  colSpan={2}>
+							<Typography variant='overline' fontWeight={700}>ACTIONS</Typography>
+						</TableCell>
 					</TableRow>
 				</TableHead>
-				<TableBody>
+				<TableBody className='crudtbl-body'>
 					{data.map((item, i) => (
-						<TableRow key={String(item[index])}
-								  className={i % 2 === 1 ? 'crudtbl-dark-row' : 'crudtbl-row'} 
-						>
-
+						<TableRow key={String(item[index])}  className={i % 2 === 1 ? 'crudtbl-dark-row' : 'crudtbl-row'} >
+						
+							{/* -- Iterate Thru All The Keys Of Each Object -- */}
 							{Object.keys(item).map((key) => key !== index && (
 								<TableCell key={key}
 										   style={{borderRight:'1px solid #808080'}}
@@ -61,6 +129,7 @@ function CRUDTable<T extends DataItemType>({data, index}:CRUDTableProps<T>)	{
 								</TableCell>
 							))}
 
+							{/* -- Actions Table Cell For Every Entry --  */}
 							<TableCell
 							 align='right'
 							 colSpan={2}
@@ -68,6 +137,7 @@ function CRUDTable<T extends DataItemType>({data, index}:CRUDTableProps<T>)	{
 							 >
 								<Stack  justifyContent={'flex-end'} direction={"row"}>
 
+									{/* -- Basic Crud Buttons + Additional Actions -- */}
 									<Tooltip title="View Details">
 										<IconButton color='info'>
 											<ViewList />
@@ -80,13 +150,31 @@ function CRUDTable<T extends DataItemType>({data, index}:CRUDTableProps<T>)	{
 										</IconButton>
 									</Tooltip>
 
-									<Tooltip title='More Actions...' >
+									
+
+									{/* -- More Actions Dropdown Button & Menu -- */}
+
+									<Tooltip title='More Actions' >
 										<Button variant='text' 
+											onClick={handleMoreActionsMenuClick}
 											startIcon={<MoreHoriz/>} 
 											endIcon={<ArrowDropDown />} 
 											color='primary'>
 										</Button>
 									</Tooltip>
+
+									<MoreActionsMenu 
+										anchorEl={moreActionsMenuAnchor}
+										open={moreActionsMenuOpen}
+										onClose={handleCloseMoreActionsMenu}
+									>
+										{additionalActions}
+										{/* -- Menu Items For Additonal Items --  */}
+
+									</MoreActionsMenu>
+
+									{/* -- More Actions Dropdown Button & Menu End -- */}									
+							
 
 									<Tooltip title="Delete Entry">
 										<IconButton color='error'>
